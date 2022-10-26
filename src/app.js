930a17cfdd9ddc55eb8596891e26bf38ea83017a
Joaquín Galeano <49453947+galeanojoaquin@users.noreply.github.com>
2022-10-26 14:43:05 +0000

    Add nodemailer, mailing.routes.js, config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express"
 import morgan from "morgan"
+var cors = require('cors')
+//Routes
+import mailingRoutes from "./routes/mailing.routes";
 
 const app = express();
 
@@ -8,8 +11,11 @@ app.set("port", 4000);
 
 // Middelwares
 
+app.use(cors())
 app.use(morgan("dev")); 
 
+//Routes
+app.use(mailingRoutes);
 
 
 export default app;
---
+++ src/routes/mailing.routes.js
@@ -0,0 +1,63 @@
+const { Router } = require("express");
+const path = require("path");
+const router = Router();
+const nodemailer = require("nodemailer");
+var hbs = require("nodemailer-express-handlebars");
+
+router.post("/send-mail", async (req, res) => {
+  console.log(req.body);
+  const { nombre, documento, telefono, email, tramite, fecha, horario, sede, codigo } =
+    req.body;
+
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve("./src/assets/html"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./src/assets/html"),
+    extName: ".handlebars",
+  };
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "licencias.miituzaingo@gmail.com",
+      pass: "incpugpmdheptjpg",
+    },
+  });
+
+  transporter.use("compile", hbs(handlebarOptions));
+
+  const mailOptions = {
+    from: "Municipalidad de ituzaingó <municipalidad@miituzaingo.gob.ar>", // sender address,
+    to: email,
+    subject: "Municipalidad de Ituzaingó | Confirmación de turno",
+    template: "email",
+    context: {
+      nombre: nombre,
+      documento: documento,
+      telefono: telefono,
+      email: email,
+      tramite: tramite,
+      fecha: fecha,
+      horario: horario,
+      sede: sede,
+      codigo: codigo,
+    },
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Email has been sent successfully!🚀");
+    }
+  });
+});
+
+
+module.exports = router;
