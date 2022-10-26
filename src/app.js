f8927c62c9ba5c70f840ba28f2aa533453b70b99
Joaquín Galeano <49453947+galeanojoaquin@users.noreply.github.com>
2022-10-26 13:08:49 +0000

    Inicializando API

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import morgan from "morgan"
+
+const app = express();
+
+//Settings
+app.set("port", 4000);
+
+// Middelwares
+
+app.use(morgan("dev")); 
+
+
+
+export default app;
